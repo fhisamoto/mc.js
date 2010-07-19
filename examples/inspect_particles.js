bc1e0841efd9df39718496c9a6b49a4c5098dcff
Fabio Hisamoto <fhisamoto@gmail.com>
2010-07-19 02:44:59 -0300

    inspect particles example

---
+++ examples/inspect_particles.js
@@ -0,0 +1,17 @@
+var sys = require('sys');
+
+var core = require('../lib/core');
+var rotations = require('../lib/rotations');
+
+console.log("new nucleus C_24");
+var nuc = new core.Nucleus(24,12);
+
+nuc.each_nucleon(function(i, nucleon) { 
+	console.log(sys.inspect(nucleon)); 
+});
+
+console.log("new p33 - 1232");
+var p33 = new core.Particle('p33_0');
+console.log(sys.inspect(p33));
+
+
