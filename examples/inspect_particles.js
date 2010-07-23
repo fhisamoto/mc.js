96c012ae840f74f4e71cc39eddd376bc6df101e4
Fabio Hisamoto <fhisamoto@gmail.com>
2010-07-23 00:45:57 -0300

    moved contants

--- examples/inspect_particles.js
+++ examples/inspect_particles.js
@@ -5,9 +5,12 @@ var core = require('../lib/core');
 console.log("new nucleus C_24");
 var nuc = new core.Nucleus(24,12);
 
+var a = 0;
 nuc.each_nucleon(function(i, nucleon) { 
-	console.log(sys.inspect(nucleon)); 
+	console.log(sys.inspect(nucleon));
+	a += 1;
 });
+console.log(a);
 
 console.log("new p33 - 1232");
 var p33 = new core.Particle('p33_0');
