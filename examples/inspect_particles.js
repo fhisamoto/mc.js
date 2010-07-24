aa10e3e01e64953e7ee2210f5c5bea8ca5dca3de
Fabio Hisamoto <fhisamoto@gmail.com>
2010-07-23 22:33:32 -0300

    jaguara each nao existe mais

--- examples/inspect_particles.js
+++ examples/inspect_particles.js
@@ -1,18 +1,20 @@
 var sys = require('sys');
-
 var core = require('../lib/core');
 
 console.log("new nucleus C_24");
-var nuc = new core.Nucleus(24,12);
+var nuc = core.NucleusBuilder(24,12);
 
 var a = 0;
-nuc.each_nucleon(function(i, nucleon) { 
-	console.log(sys.inspect(nucleon));
+nuc.each_nucleon(function(nucleon) {
+	console.log("name:" + nucleon.name);
+	console.log("p:" + nucleon.p)
+	console.log("pos:" + nucleon.pos);
+	console.log("");
 	a += 1;
 });
-console.log(a);
+console.log("nucleons: " + a);
 
-console.log("new p33 - 1232");
+console.log("inspect p33 - 1232");
 var p33 = new core.Particle('p33_0');
 console.log(sys.inspect(p33));
 
